a190b1e345d7a16cabb14a40983cdba247fb57c0
Jegan J <jegan.jeyaraman@sellerdeck.com>
2022-10-04 20:37:40 +0530

    Initial committing to add theme files

---
+++ magento/app/design/frontend/McFadyen/theme/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Copyright © 2022 McFadyen. All rights reserved.
+*/
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/McFadyen/theme',
+    __DIR__
+);
