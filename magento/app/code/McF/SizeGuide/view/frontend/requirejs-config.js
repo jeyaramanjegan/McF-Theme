45ecd95d27455805e3a7e7edb813787b4faefb32
Jegan Jeyaraman <48308523+jeyaramanjegan@users.noreply.github.com>
2022-10-25 12:46:57 +0530

    Update and rename magento/app/code/McFadyen/SizeGuide/view/frontend/requirejs-config.js to magento/app/code/McF/SizeGuide/view/frontend/requirejs-config.js
    changes for structure

--- magento/app/code/McF/SizeGuide/view/frontend/requirejs-config.js
+++ magento/app/code/McF/SizeGuide/view/frontend/requirejs-config.js
@@ -6,7 +6,7 @@ var config = {
     config: {
         mixins: {
             'Magento_Swatches/js/swatch-renderer': {
-                'McFadyen_SizeGuide/js/swatch-renderer-mixin': true
+                'McF_SizeGuide/js/swatch-renderer-mixin': true
             }
         }
     }
