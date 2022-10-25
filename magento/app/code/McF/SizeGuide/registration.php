123442b42caf1b46bc1cdca059a847e5e26a57e4
Jegan J <jegan.jeyaraman@sellerdeck.com>
2022-10-25 12:53:54 +0530

    file change for theme name

--- magento/app/code/McF/SizeGuide/registration.php
+++ magento/app/code/McF/SizeGuide/registration.php
@@ -8,4 +8,4 @@ declare(strict_types=1);
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'McFadyen_SizeGuide', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'McF_SizeGuide', __DIR__);
