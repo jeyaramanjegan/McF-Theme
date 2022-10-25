82a66c5ed1cad688d90220fdcc3d24b08097bd61
Jegan J <jegan.jeyaraman@sellerdeck.com>
2022-10-25 12:51:05 +0530

    files added and deleted for my ref.

--- magento/app/design/frontend/McF/theme/registration.php
+++ magento/app/design/frontend/McF/theme/registration.php
@@ -5,6 +5,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/McFadyen/theme',
+    'frontend/McF/theme',
     __DIR__
 );
